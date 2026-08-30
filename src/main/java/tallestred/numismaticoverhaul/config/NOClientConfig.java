6238e997f0c8404a0f60b59b92782fa78e80d134
platypushasnohat <58866828+platypushasnohat@users.noreply.github.com>
2026-08-30 15:02:09 -0600

    Fix purse offset configs
    - Fixed purse offset configs not accepting negative values
    - Fixed survival purse offsets using the y offset value for both x and y

--- src/main/java/tallestred/numismaticoverhaul/config/NOClientConfig.java
+++ src/main/java/tallestred/numismaticoverhaul/config/NOClientConfig.java
@@ -25,12 +25,12 @@ public class NOClientConfig {
     public NOClientConfig(ModConfigSpec.Builder builder) {
         builder.push("client");
         example = builder.define("purse", true);
-        creativePursePositionX = builder.defineInRange("creativePurseX", 0, 0, 1000);
-        creativePursePositionY = builder.defineInRange("creativePurseY", 0, 0, 2000);
-        survivalPursePositionX = builder.defineInRange("survivalPurseX", 0, 0, 1000);
-        survivalPursePositionY = builder.defineInRange("survivalPurseY", 0, 0, 2000);
-        merchantPursePositionX = builder.defineInRange("merchantPurseX", 0, 0, 1000);
-        merchantPursePositionY = builder.defineInRange("merchantPurseY", 0, 0, 2000);
+        creativePursePositionX = builder.defineInRange("creativePurseX", 0, -1000, 1000);
+        creativePursePositionY = builder.defineInRange("creativePurseY", 0, -2000, 2000);
+        survivalPursePositionX = builder.defineInRange("survivalPurseX", 0, -1000, 1000);
+        survivalPursePositionY = builder.defineInRange("survivalPurseY", 0, -2000, 2000);
+        merchantPursePositionX = builder.defineInRange("merchantPurseX", 0, -1000, 1000);
+        merchantPursePositionY = builder.defineInRange("merchantPurseY", 0, -2000, 2000);
         builder.pop();
         builder.build();
     }
